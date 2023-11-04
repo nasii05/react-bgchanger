c8fd04123f4e7ef57263a9555bf0fecdab801194
Nasih <iamnasih@gmail.com>
2023-11-04 13:34:09 +0530

    buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="bg-green-500 text-3xl">
-        A bg Changer app with vite
-      </h1>
-    </>
+   <div className="w-full h-screen duration-200 bg-black">
+      <div className="fixed flex flex-wrap justify-center
+      bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3
+        shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full
+          text-white shadow-lg"
+          >test</button>
+        </div>
+      </div>
+   </div>
   );
 }
 
