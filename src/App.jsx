ea8a3a6f1544be24bb7aee9161bd52e238f11296
Nasih <iamnasih@gmail.com>
2023-11-04 13:56:54 +0530

    project completed and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,38 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+ const [color, setColor] = useState('olive');
 
   return (
-   <div className="w-full h-screen duration-200 bg-black">
+   <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center
       bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3
         shadow-lg bg-white px-3 py-2 rounded-3xl">
+          
           <button className="outline-none px-4 py-1 rounded-full
-          text-white shadow-lg"
-          >test</button>
+          text-black shadow-lg"
+          onClick={ () => setColor('red') }
+          style={{backgroundColor:"red"}}>Red
+          </button>
+
+          <button className="outline-none px-4 py-1 rounded-full
+          text-black shadow-lg"
+          onClick={() => setColor('green')}
+          style={{backgroundColor:'green'}}>Green
+          </button>
+
+          <button className="outline-none px-4 py-1 rounded-full
+          text-black shadow-lg"
+          onClick={() => setColor('lightblue')}
+          style={{backgroundColor:'lightblue'}}>lightblue
+          </button>
+
+          <button className="outline-none px-4 py-1 rounded-full
+          text-black shadow-lg"
+          onClick={() => setColor('lightpink')}
+          style={{backgroundColor:'lightpink'}}>lightpink
+          </button>
         </div>
       </div>
    </div>
